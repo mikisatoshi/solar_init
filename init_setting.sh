0e5f40302fde466f5c3b6656f5c7ba90cce6ec81
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2019-11-10 13:34:50 +0900

    no message

--- init_setting.sh
+++ init_setting.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 ls
 
-sudo apt install python-pip
+sudo apt update
+sudo apt install python python-dev python3 python3-dev
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+pip --version
+
 sudo pip install numpy
+sudo pip install pandas
+sudo pip install oauth2client
+sudo pip install gspread
+sudo pip install google-api-python-client
+sudo pip install PyDrive
